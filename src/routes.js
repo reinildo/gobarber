f1a0ba2310496263d1343e3b3b9907b457b82256
Reinildo <reinildo@gmail.com>
2019-10-24 23:52:55 -0300

    Started Appointments

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProvidersController from './app/controllers/ProvidersController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,5 +23,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProvidersController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
