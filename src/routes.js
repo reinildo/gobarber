e36b048b2a009878dca76bcfed999edc57d864a0
Reinildo <reinildo@gmail.com>
2019-10-26 14:03:53 -0300

    feat: Mark notification as read

--- src/routes.js
+++ src/routes.js
@@ -24,13 +24,14 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProvidersController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
